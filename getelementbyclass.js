75ceff9202b36849b5547d3ca0d90f6181d4fd10
vaishnaviaushikatr <vaishnavi.aushikar2@gmail.com>
2023-03-30 15:55:23 +0530

    getelementbyclassnameadded

---
+++ getelementbyclass.js
@@ -0,0 +1,8 @@
+var listitem=document.getElementsByClassName('list-group-item')
+//var listitem3=listitem[3]
+listitem[2].style.backgroundColor="green"
+//listitem.style.fontWeight="bold"
+for(let i=0;i<listitem.length;i++)
+{
+    listitem[i].style.fontWeight="bold"
+}
