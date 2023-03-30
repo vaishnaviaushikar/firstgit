4d2ea7f2e81c40b24027da8949c4177332e35bc0
vaishnaviaushikatr <vaishnavi.aushikar2@gmail.com>
2023-03-30 08:23:02 +0530

    js file added

---
+++ sharpener/dom.js
@@ -0,0 +1,10 @@
+var title=document.getElementById("header-title");
+title.textContent="List of Items";
+//title.style.borderBottom='solid 3px black';
+// var header=document.getElementsByClassName("bg-success");
+// header.style.borderBottom= 'solid 3px black'
+title.style.color='yellow'
+title.style.backgroundColor='#68DE52'
+title.style.border='3px solid black';
+
+document.getElementById("main").style.color = "green";
