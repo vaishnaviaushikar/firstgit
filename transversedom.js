ec5db542b0749b0152530862d2a4c6e4c122fc0e
vaishnaviaushikatr <vaishnavi.aushikar2@gmail.com>
2023-03-31 15:22:24 +0530

    transvereDOM file added

---
+++ transversedom.js
@@ -0,0 +1,45 @@
+var list=document.querySelector('#items')
+// //PARENTELEMENT--
+// console.log(list.parentElement)
+
+// //LASTELEMENTCHILD
+// console.log(list.lastChild)
+// console.log(list.lastElementChild)
+
+// //CREATE-CHILD
+
+let newdiv=document.createElement('div')
+// console.log(newdiv)
+
+// //FIRSTELEMENTCHILD--
+// console.log(list.firstChild)
+// console.log(list.firstElementChild)
+
+// //NEXTSIBLING--
+// console.log(list.nextSibling)
+// console.log(list.nextElementSibling)
+
+// //PREVIOUSSIBLING--
+// console.log(list.previousSibling)
+// console.log(list.previousElementSibling)
+
+// //CREATEELEMENT--
+var newelement=document.createTextNode("HEllo");
+newdiv.appendChild(newelement)
+// console.log(newdiv)
+
+// //SET ATTRIBUTE--
+newdiv.setAttribute('title','hello div')
+//console.log(newdiv);
+
+//ADD BEFORE--
+
+var container=document.querySelector("header .container");
+var h1=document.querySelector("header #header-title")
+container.insertBefore(newdiv,h1)
+//console.log(h1)
+
+var classname=document.querySelector('.list-group')
+var item1=document.querySelector('.list-group-item')
+classname.insertBefore(newdiv,item1)
+//console.log(item1)
