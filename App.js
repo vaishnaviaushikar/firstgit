25c22d70b11f108dcd5b30fd9803d01c0a1846d0
vaishnaviaushikatr <vaishnavi.aushikar2@gmail.com>
2023-06-15 08:05:13 +0530

    react-passingdata-loopingexpenses

---
+++ App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ExpenseItem  from './Components/ExpenseItem';
+
+function App() {
+
+  const expenses=[
+    {
+      title:'Car Insurance',
+      amount:6454,
+      date:new Date(2022,10,20),
+      location:"New Delhi"
+    },
+{title:'Petrol',
+amount:3223,date:new Date(2022,7,20),location:"HP petrolium"},
+{title:'Grocery',
+amount:4332,date:new Date(2021,8,29),location:"Wall street"},
+{title:'Rent',
+amount:3442,date:new Date(2020,10,20),location:"sp galaxy"},
+{
+  title:'other',amount:4634,date:new Date(2022,5,7),location:"graffity"
+}
+]
+
+  
+return (
+  <div>
+    <h2>Lets get started!</h2>
+
+    {expenses.map((expense, index) => (
+      <ExpenseItem
+        key={index}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        location={expense.location}
+      />
+    ))}
+  </div>
+);
+}
+
+export default App;
