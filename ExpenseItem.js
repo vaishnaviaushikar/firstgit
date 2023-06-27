829403ef453e5db860755c7a451da9a858edc032
vaishnaviaushikatr <vaishnavi.aushikar2@gmail.com>
2023-06-27 15:29:44 +0530

    Delete Expense Button Added

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,11 +1,21 @@
-function ExpenseItem(){
-    return (
-        <div>
-    <h2>Expense item!</h2>
-    <p>Food Rs 10</p>
-    <p>Petrol Rs 100</p>
-    <p>Movies Rs 200</p>
-    </div>
-    )
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem=(props)=>{
+
+
+  return (
+        <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+        <div className="expense-item__description">
+            <h2> {props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
+            <div className="expense-item__location">{props.location}</div>
+        </div>
+        <button >Change Title</button>
+        <button>Delete Expense</button>
+        </Card>
+    );
 }
 export default ExpenseItem;
